0fedb3085e6bf1dcb29483e351f55e8bb80ed160
merlinlyc <merlinlyc@gmail.com>
2016-04-07 16:54:56 +0800

    add css add missing views

--- app/controllers/forum_threads_controller.rb
+++ app/controllers/forum_threads_controller.rb
@@ -1,2 +1,26 @@
-class ForumThread
+class ForumThreadsController < ApplicationController
+  before_action :authenticate_user!, except:  [:index, :show]
+  before_action :set_forum_thread, except:  [:index, :new, :create]
+
+  def index
+    @forum_threads = ForumThread.all
+  end
+
+  def show
+
+  end
+
+  def new
+    @forum_threads = ForumThread.new
+    @forum_threads.forum_posts.new
+  end
+
+  def create
+
+  end
+
+  private
+    def set_forum_thread
+      @forum_threads = ForumThread.find(params[:id])
+    end
 end
