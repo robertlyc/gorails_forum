5e411ec63ec79c86be0f980a0fcef84d87daa1f5
merlinlyc <merlinlyc@gmail.com>
2016-04-19 13:37:35 +0800

    add email notification

---
+++ app/mailers/application_mailer.rb
@@ -0,0 +1,4 @@
+class ApplicationMailer < ActionMailer::Base
+  default from: "notifications@gorails.com"
+  layout 'mailer'
+end
--- app/models/forum_post.rb
+++ app/models/forum_post.rb
@@ -16,4 +16,12 @@ class ForumPost < ActiveRecord::Base
 
   validates :body, presence: true
 
+  def send_notifications!
+    users = forum_thread.users.uniq - [user]
+    users.each do |user|
+      NotificationMailer.forum_post_notification(user, self).deliver_later
+    end
+
+  end
+
 end
